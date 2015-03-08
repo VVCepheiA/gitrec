0d634c197dcb21bfdbf26cbc8ea81302020aeebe
VVCepheiA <oyevera@gmail.com>
2015-03-08 09:41:14 -0400

    DOESN'T WORK

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-
+var bodyParser = require('body-parser');
 //DB
 var mongoose = require('mongoose');
 var dbName = 'gitrec';
